a7273b56adfa732fbcacaf42fd024d0463da4af3
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-08-12 14:40:45 +0200

    prueba técnica

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,17 @@
+import axios from 'axios';
 import React, {useRef,useState} from 'react'
+import noEncontrado from '../assets/images/noEncontrado.jpg'
+const URL_SEARCH='http://openlibrary.org/search.json?q='
+const URL_COVER='https://covers.openlibrary.org/b/id/'
+const COVER_SIZE='-M.jpg'
 
 const Search=()=>{
     const titulo = useRef(null);
     //Cogemos el valor del input en un state
     const [valorEscogido, setValorEscogido]=useState("")
-    const [title, setTitle]=useState("")
-    const [autor, setAutor]=useState("")
-    const [fecha, setFecha]=useState("")
-    const [portada, setPortada]=useState("")
+    //Resultado del array de la petición en un state
+    const [dato, setDato]=useState([])
+    
     
     const handleChange=()=>{
         //Cogemos los valores del input a través de useRef y con el evento onChange
@@ -21,54 +25,66 @@ const Search=()=>{
             newValue+=letra;
         }
         setValorEscogido(newValue)
-        console.log("El valor del input es: "+valorEscogido)
         apiLibros()
     }
 
     
         const apiLibros=()=>{
-            fetch('http://openlibrary.org/search.json?q='+valorEscogido)
-            .then((res)=>res.json())
-            .then((data)=>{
-                
-                setTitle(data.docs[0].title)
-                setAutor("Autor: "+data.docs[0].author_name)
-                setFecha("Primer año de publicación: "+data.docs[0].first_publish_year)
-                setPortada("https://covers.openlibrary.org/b/isbn/"+data.docs[0].isbn+"-S.jpg")
-                console.log("Título: "+data.docs[0].title)
-                console.log("Autor: "+data.docs[0].author_name)
-                console.log("Primer año de publicación: "+data.docs[0].first_publish_year)
-                console.log("Portada: "+data.docs[0].isbn)
-                
+            axios.get(URL_SEARCH+valorEscogido)
+            .then((res)=>{
+                //filtra los libros con una sóla edición
+                setDato(res.data['docs'].filter(book=>book.edition_count===1))
             })
+
         };
         
     
     return(
         <div className="containerBuscador">
+        
             <h1>Buscador de libros</h1>
             <div className="input-group rounded" id="buscador">
+                <p>
+                    <label htmlFor='busqueda'>Introduce un libro &nbsp;&nbsp;</label>
+                </p>
                 
-                
-                <input type="search"
+                    <input type="search"
                         ref={titulo}
                         onChange={handleChange}
                         className="form-control rounded"
                         placeholder="Search" 
                         aria-label="Search" 
-                        aria-describedby="search-addon" />
-                <span className="input-group-text border-0" id="search-addon" onClick={handleChange}>
-                    <i className="fas fa-search" ></i>
-                </span>
+                        aria-describedby="search-addon"
+                        required />
+                    <span className="input-group-text border-0" id="search-addon" onClick={handleChange}>
+                        <i className="fas fa-search" ></i>
+                    </span>
+                
+                
             </div>
 
             <div id="resultado">
-                        <div className="jumbotron">
-                            <h1 className="display-4">{title}</h1>
-                            <p className="lead">{autor}</p>
+                {dato.map((i)=>{
+                    /*Se imprimen los resultados en un menú horizontal que se puede hacer scroll con el ratón*/
+                    return(
+                        <div className="jumbotron" key={i.key} id="hijoResultado">
+                            
+                            <h1 className="display-4">{i.title}</h1>
+                            <p className="lead">{i.author_name}</p>
                             <hr className="my-4"></hr>
-                            <p>{fecha}</p>
+                            <p>{i.first_publish_year}</p>
+                            {(()=>{
+                                if (i.cover_i==null)
+                                    return <img src={noEncontrado}  width="200" height="200" alt="cover"/>
+                                else 
+                                    return <img src={URL_COVER+i.cover_i+COVER_SIZE} alt="cover"/>
+                            })()}
+                            
                         </div>
+                    )
+                })
+                }
+                        
             </div>
         </div>
     )
